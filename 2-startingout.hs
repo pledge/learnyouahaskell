cc97e7bcbd0ad94a1a32aec0060db5834539fce0
Martin Gilday <martin@gilday.me>
2010-12-29 15:48:04 +0000

    Working through chapter two

---
+++ 2-startingout.hs
@@ -0,0 +1,36 @@
+{-
+	This file will not compile.  
+	They are expressions for use in GHCi.
+-}
+
+-- arithmetic
+
+2 + 15
+49 * 100
+1892 - 1472
+5/2
+
+(50 * 100) - 4999
+
+-- negative numbers are best written within paratheneses 
+5 * (-3)
+
+-- Truth
+
+True && False -- False
+True && True -- True
+True || False -- True
+not False -- True
+not (True && True) -- False
+
+-- Equality
+5 == 5 -- True
+5 /= 5 -- False
+"hello" == "hello" -- True
+
+-- Functions
+succ 8 -- 9
+min 9 10
+max 100 101
+
+
