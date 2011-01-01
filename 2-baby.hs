f4aa3d4f78902bf74827a2c27446f86842e875fd
Martin Gilday <martin@gilday.me>
2011-01-01 12:59:20 +0000

    Completed chapter two

--- 2-baby.hs
+++ 2-baby.hs
@@ -43,4 +43,32 @@ maxItem = maximum [1,9,2,3,4]
 
 isElement = 4 `elem` [3,4,5,6] -- truth of whether item in list (contains)
 
+range = [1..20]
+evens = [2,4..20]
+decRange = [20..1]
+letters = ['a'..'z']
 
+lazyRange = take 24 [13,26..]
+
+cycleList = take 10 (cycle [1,2,3])
+repeatList = take 10 (repeat 5)
+replicateList = replicate 3 10
+
+firstTenEven = [x*2 | x <- [1..10]]
+firstTenEven' = [x*2 | x <- [1..10], x*2 >= 12]
+boomBangs xs = [if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
+boomBangs' xs = [if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
+
+products = [x*y | x<-[2,5,10], y<-[8,10,11]]
+products' = [x*y | x<-[2,5,10], y<-[8,10,11], x*y > 50]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [c |  c <- st, c `elem` ['A'..'Z']]
+
+tupleList = [(1,2), (3,4), (5,6)]
+firstItemInPair = fst (8,11) -- fst and snd only work on pairs, not triples etc
+secondItemInPair = snd (8,11)
+
+zippedList = zip [1,2,3,4,5] [5,5,5,5,5]
+zippedList' = zip [1,2,3,4,5] (replicate 5 5)
